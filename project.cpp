33aa9dc3e0ee8f27d97fcbba435d1d024a9e4dbf
Marlus Marcos <marlusufrn@gmail.com>
2019-04-23 11:48:52 -0300

    comitando..

---
+++ project.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+/**
+*@Função que calcula o fatorial de um número
+*@author Marlus Marcos
+*@param n Número no qual o fatorial será declarado
+*@return o fatorial  de um número
+*/
+long double fatorial (long double n);

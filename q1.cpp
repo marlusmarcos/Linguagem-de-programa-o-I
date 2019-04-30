6995741990e50469e68673b475adc72730630729
marlusmarcos <38912986+marlusmarcos@users.noreply.github.com>
2019-04-30 15:48:24 -0300

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,27 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+using namespace std;
 class Funcionario //classe que representa um funciuonario
 {
+private:
+	string nome;
+	float salario;
+	string data_contrato;
 public:
-	std::string nome;
-	double salario;
-	std::string data_contrato;
-
+	//Funcionario::Funcionario (string n, float s);
+	void setNome (string s);
+	string getNome ();
+	void setSalario (float salario);
+	float getSalario ();
 
 };
+
+void Funcionario::setNome (string s) {
+    nome = s;
+}
+Funcionario::getNome() {
+    return nome;    
+}
+
+void Funcionario::setSalario (float s_) {
+	salario = s_;
+}
+
+Funcionario::getSalario () {
+	return salario;
+}
+
 
 class Empresa //classe que representa a empresa
 {
 public:
-	std::string seu_nome;
-	std::string cnpj;
-	std::vector<Funcionario> empregados; /* a empresa tem que ter uma lista de funcionários
+	string seu_nome;
+	string cnpj;
+	vector<Funcionario> empregados; /* a empresa tem que ter uma lista de funcionários
 	então criei um vector do tipo <Funcionario> para guardar nele.
 	*/
 	Empresa() { //incicializei o contrutor, assim inicializando o vector empregados
-		std::vector<Funcionario> empregados; // mas o compilador diz que está errado e não estou conseguindo sair disso.
+		vector<Funcionario> empregados; // mas o compilador diz que está errado e não estou conseguindo sair disso.
 	}
 	void adicionarFuncionario(Funcionario f) { // aqui a função para adicionar, mas o "empregados" não está funcionando
 	
@@ -29,41 +50,40 @@ public:
 	}
 	//~Empresa();	
 };
-/*class Gerenciar
+class Gerenciar
 {
-	std::string inicial; //classe que serve para gerenciar as empresas.
-	std::vector<Empresa> lista_empresa;
-
+	string inicial; //classe que serve para gerenciar as empresas.
+	vector<Empresa> lista_empresa;
 	Gerenciar () {
-		std::vector <Empresa> lista_empresa;
+		vector <Empresa> lista_empresa;
 	}
 	void adicionarEmpresa (Empresa e) {
 		lista_empresa.push_back(e);
 	}
-};*/
+};
 void print_nome_salario (Funcionario n_func) {
-	std::cout << "Nome do Funcionario: " << n_func.nome << std::endl;
-	std::cout << "Nome do Funcionario: " << n_func.salario << std::endl;
+	cout << "Nome do Funcionario: " << n_func.nome << endl;
+	cout << "Nome do Funcionario: " << n_func.salario << endl;
 };
 
 int main(int argc, char const *argv[])
 {
-	std::vector<Empresa> lista_empresa;
-	std::cout << "Testando o funcionamento\n";
+	vector<Empresa> lista_empresa;
+	cout << "Testando o funcionamento\n";
 	//criando funcionário --- teste ---
 	Funcionario f1;
 	f1.nome = "Marlus Marcos";
 	f1.salario = 4.855;
-	std::cout << "nome do func: " << f1.nome <<"\nSalaŕio do func: " << f1.salario << std::endl;
-	std::cout << "======================\n";
+	cout << "nome do func: " << f1.nome <<"\nSalaŕio do func: " << f1.salario << endl;
+	cout << "======================\n";
 
 	//criando empresa
-	std::cout << "Testando Empresa\n";
+	cout << "Testando Empresa\n";
 	Empresa microsoft;
 	microsoft.seu_nome = "Microsoft";
 	microsoft.cnpj = "132545";
 	microsoft.adicionarFuncionario(f1);
-	std::cout << "seu nome: " << microsoft.seu_nome<< "\n" << "cnpj: " << microsoft.cnpj << std::endl;
+	cout << "seu nome: " << microsoft.seu_nome<< "\n" << "cnpj: " << microsoft.cnpj << endl;
 	lista_empresa.push_back (microsoft);
 
 
@@ -71,21 +91,19 @@ int main(int argc, char const *argv[])
 	while (i == 1) {
 		Empresa a;
 		Funcionario b;
-		std::string _nome;
+		string _nome;
 		float sal;
-		std::cout << "Digite o nome do func e seu salario\n";
-		std::cin >> _nome;
-		std::cin >> sal;
+		cout << "Digite o nome do func e seu salario\n";
+		cin >> _nome;
+		cin >> sal;
 		b.nome = _nome;
 		b.salario = sal;
 		print_nome_salario(b);
 		a.adicionarFuncionario(b);	
-		std::cout << "funcionário adicionado a empresa: " << a.seu_nome << std::endl;
-		std::cout << "Digite 1 para continuar ou 0 para sair\n";
-		std::cin >> i;
+		cout << "funcionário adicionado a empresa: " << a.seu_nome << endl;
+		cout << "Digite 1 para continuar ou 0 para sair\n";
+		cin >> i;
 	}
-
-
 
 	return 0;
 }

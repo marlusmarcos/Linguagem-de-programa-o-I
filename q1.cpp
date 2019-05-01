1a221291c779ee57148abcef8783da45c93f0e1a
marlusmarcos <38912986+marlusmarcos@users.noreply.github.com>
2019-05-01 20:05:38 -0300

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -12,15 +13,16 @@ public:
 	//Funcionario::Funcionario (string n, float s);
 	void setNome (string s);
 	string getNome ();
-	void setSalario (float salario);
+	void setSalario (float s_);
 	float getSalario ();
+	void print_nome_salario ();
 
 };
 
 void Funcionario::setNome (string s) {
     nome = s;
 }
-Funcionario::getNome() {
+string Funcionario::getNome() {
     return nome;    
 }
 
@@ -28,28 +30,58 @@ void Funcionario::setSalario (float s_) {
 	salario = s_;
 }
 
-Funcionario::getSalario () {
+float Funcionario::getSalario () {
 	return salario;
 }
 
 
 class Empresa //classe que representa a empresa
 {
-public:
+private:
 	string seu_nome;
 	string cnpj;
 	vector<Funcionario> empregados; /* a empresa tem que ter uma lista de funcionários
 	então criei um vector do tipo <Funcionario> para guardar nele.
 	*/
-	Empresa() { //incicializei o contrutor, assim inicializando o vector empregados
-		vector<Funcionario> empregados; // mas o compilador diz que está errado e não estou conseguindo sair disso.
-	}
-	void adicionarFuncionario(Funcionario f) { // aqui a função para adicionar, mas o "empregados" não está funcionando
+public:
+	Empresa(); //incicializei o contrutor, assim inicializando o vector empregados
+	 // mas o compilador diz que está errado e não estou conseguindo sair disso.
 	
-		empregados.push_back(f);
-	}
+    void setSeu_nome (string n_empresa); //setSeu_nome representa o nome da classe empresa
+	string getSeu_nome();
+	void setCnpj(string n_cnpj);
+	string getCnpj ();
+
+	void adicionarFuncionario(Funcionario *f); // aqui a função para adicionar, mas o "empregados" não está funcionando
+	void print_nome_cnpj ();
 	//~Empresa();	
 };
+    Empresa::Empresa () {
+	    vector<Funcionario> empregados;
+	}
+    void Empresa::adicionarFuncionario(Funcionario *f) { // aqui a função para adicionar, mas o "empregados" não está funcionando
+	
+		empregados.push_back(*f);
+	}
+	void Empresa::setSeu_nome (string n_empresa) {
+		seu_nome = n_empresa;
+	}
+	string Empresa::getSeu_nome () {
+		return seu_nome;
+	}
+	void Empresa::setCnpj(string n_cnpj) {
+		cnpj = n_cnpj;
+
+	}
+	string Empresa::getCnpj () {
+		return cnpj;
+	};
+    void Empresa::print_nome_cnpj () {
+		cout << "O nome da empres é: " << this->seu_nome << endl;
+		cout << "O CNPJ da empresa é" << this->cnpj << endl;
+	}
+
+
 class Gerenciar
 {
 	string inicial; //classe que serve para gerenciar as empresas.
@@ -61,9 +93,9 @@ class Gerenciar
 		lista_empresa.push_back(e);
 	}
 };
-void print_nome_salario (Funcionario n_func) {
-	cout << "Nome do Funcionario: " << n_func.nome << endl;
-	cout << "Nome do Funcionario: " << n_func.salario << endl;
+void Funcionario::print_nome_salario () {
+	cout << "Nome do Funcionario: " << this->nome << endl;
+	cout << "Salario do Funcionario: " << this->salario << endl;
 };
 
 //fazer alterações na main, pois esse código abaixo está com as funções antigas, porque os métodos agora são privados
@@ -74,39 +106,39 @@ int main(int argc, char const *argv[])
 	vector<Empresa> lista_empresa;
 	cout << "Testando o funcionamento\n";
 	//criando funcionário --- teste ---
-	Funcionario f1;
-	f1.setNome = "Marlus Marcos";
-	f1.setSalario = 4.855;
-	cout << "nome do func: " << f1.getNome <<"\nSalaŕio do func: " << f1.getSalario << endl;
+	Funcionario *f1 = new Funcionario;
+	f1->setNome("Marlus arcos");
+	f1->setSalario(4.855);
+	f1 -> print_nome_salario();
 	cout << "======================\n";
 
 	//criando empresa
 	cout << "Testando Empresa\n";
-	Empresa microsoft;
-	microsoft.seu_nome = "Microsoft";
-	microsoft.cnpj = "132545";
-	microsoft.adicionarFuncionario(f1);
-	cout << "seu nome: " << microsoft.seu_nome<< "\n" << "cnpj: " << microsoft.cnpj << endl;
-	lista_empresa.push_back (microsoft);
+	Empresa *microsoft = new Empresa;
+	microsoft->setSeu_nome("Microsoft");
+	microsoft->setCnpj("132545");
+	microsoft->adicionarFuncionario(f1);
+	microsoft->print_nome_cnpj();
+	lista_empresa.push_back (*microsoft);
 
-
+    Empresa *a = new Empresa;
+	a->setSeu_nome ("Professores efetivos IFRN");
 	int i=1;
 	while (i == 1) {
-		Empresa a;
-		Funcionario b;
+		Funcionario *b = new Funcionario;
 		string _nome;
 		float sal;
 		cout << "Digite o nome do func e seu salario\n";
 		cin >> _nome;
 		cin >> sal;
-		b.nome = _nome;
-		b.salario = sal;
-		print_nome_salario(b);
-		a.adicionarFuncionario(b);	
-		cout << "funcionário adicionado a empresa: " << a.seu_nome << endl;
+		b->setNome (_nome);
+		b->setSalario (sal);
+		b->print_nome_salario();
+		a->adicionarFuncionario(b);	
+		cout << "funcionário adicionado a empresa: " << a->getSeu_nome() << endl;
 		cout << "Digite 1 para continuar ou 0 para sair\n";
 		cin >> i;
 	}
-
+/**/
 	return 0;
 }

4bcf1b83ab4cc80215069ff24f550bf20f791a79
marlusmarcos <38912986+marlusmarcos@users.noreply.github.com>
2019-06-25 21:03:19 -0300

    Update testearquivo.cpp

--- testearquivo.cpp
+++ testearquivo.cpp
@@ -47,68 +47,104 @@ void Funcionario::listar(Funcionario *f1) {
     cout << "Funcionario " << f1->getNome() << endl;
 }
 
-////Funcionario::Funcionario(){};
-//Funcionario::~Funcionario(){};
-
-int main () {
-    string nome;
-    int idade;
-    Funcionario *f1 = new Funcionario();
-    vector<Funcionario> vector_func;
-	fstream arquivo("lista.csv", ios::in); // aqui abro o arquivo em modo leitura
-	string nome1;
-	string idade1;
-
-    int i = -4;
-
-    //fstream arquivo2;
-
- 	while (arquivo.good()) {
+class vector_de_funcionario {
+    protected:
+      vector<Funcionario> vector_func;
+    public:
+        void  carregarFunc();
+        void  cadastraFunc();
+        void atualizar();
+};
+    void vector_de_funcionario::carregarFunc () {
+        string nome;
+        int idade;
+        Funcionario *f1 = new Funcionario();
+    	fstream arquivo("lista.csv", ios::in); // aqui abro o arquivo em modo leitura
+    	string nome1;
+    	string idade1;
+     	while (arquivo.good()) {
         getline (arquivo, nome1,';');
         getline (arquivo,idade1,'\n');
-
         f1->setNome(nome1);
         idade = atoi(idade1.c_str());
         f1->setIdade(idade);
         vector_func.push_back(*f1);
-    }
-    arquivo.close(); // fecho ele
-
-    while (i != 0) {
-        cout << "Digite 0 para sair, 1 para cadastrar e 2 para listar\n";
-        cin >> i;
-        switch(i) {
-    case 0:
-        i = 0;
-        break;
-
-    case 1:
-        cout <<"Digite seu nome: \n";
-        cin >> nome;
-        cout << "Digite sua idade \n";
-        cin >> idade;
-        f1->setNome(nome);
-        f1->setIdade(idade);
-        vector_func.push_back(*f1);
-        if (arquivo.is_open()) { // se estiver aberto, add
-            arquivo << "\n" << nome << ";" << idade;
-        } else {
-            arquivo.open("lista.csv", ios::out|ios::app); // se n, abro e não sobre escvreve
-            arquivo << "\n" << nome << ";" << idade;
         }
-        break;
-    case 2:
-        int tam = vector_func.size();
-        for (int k=0; k < tam ; ++k) {
-            *f1=vector_func[k];
-            cout << f1->getNome() << ", " << f1->getIdade() << "\n";
-        }
-        break;
-
+        arquivo.close(); // fecho ele
     }
-}
+
+    void vector_de_funcionario::cadastraFunc () {
+        int i = -1;
+        string nome;
+        int idade;
+        Funcionario *f1 = new Funcionario();
+        fstream arquivo;
+        while (i != 0) {
+            cout << "Digite 0 para sair, 1 para cadastrar e 2 para listar, 3 para alterar\n";
+            cin >> i;
+            switch(i) {
+                case 0:
+                    i = 0;
+                    break;
+
+                case 1:
+                    cout <<"Digite seu nome: \n";
+                    cin >> nome;
+                    cout << "Digite sua idade \n";
+                    cin >> idade;
+                    f1->setNome(nome);
+                    f1->setIdade(idade);
+                    vector_func.push_back(*f1);
+                    if (arquivo.is_open()) { // se estiver aberto, add
+                        arquivo << "\n" << nome << ";" << idade;
+                    } else {
+                        arquivo.open("lista.csv", ios::out|ios::app); // se n, abro e não sobre escvreve
+                        arquivo << "\n" << nome << ";" << idade;
+                    }
+                    break;
+                case 2:{
+                    int tam = vector_func.size();
+                    for (int k=0; k < tam; ++k) {
+                        *f1=vector_func[k];
+                        cout << f1->getNome() << ", " << f1->getIdade() << "\n";
+                    }
+                    break;
+                    }
+                case 3:
+                    int k2;
+                    cout << "Digite o id do funcionario a alterar, 0,1,2,...,n-1\n";
+                    cin >> k2;
+                    cout <<"Digite seu nome: \n";
+                    cin >> nome;
+                    cout << "Digite sua idade \n";
+                    cin >> idade;
+                    f1->setNome(nome);
+                    f1->setIdade(idade);
+                    vector_func[k2] = *f1;
+                    break;
+            }
+    }
 
     arquivo.close();
+}
 
+void vector_de_funcionario::atualizar() {
+    auto i=0;
+    int tam = vector_func.size();
+    Funcionario *f1 = new Funcionario();
+    fstream arquivo;
+    arquivo.open("lista.csv", ios::out);
+    for (i=0;i<tam; ++i){
+        *f1=vector_func[i];
+        arquivo << f1->getNome() << ", " << f1->getIdade() << "\n";
+    }
+
+}
+
+int main () {
+    vector_de_funcionario f2;
+    f2.carregarFunc();
+    f2.cadastraFunc();
+    f2.atualizar();
 	return 0;
 }
